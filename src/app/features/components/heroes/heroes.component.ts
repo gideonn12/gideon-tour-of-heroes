6abc202dd86c3864ad7d8a87b6ad499f8bb12866
gideonn12 <gideoneeman@icloud.com>
2026-03-05 12:26:00 +0200

    Show Hero Equipment

--- src/app/features/components/heroes/heroes.component.ts
+++ src/app/features/components/heroes/heroes.component.ts
@@ -13,6 +13,9 @@ import { NgStyle } from "@angular/common";
 import { loadTeams } from "../../../store/teams/actions";
 import { selectAllTeams } from "../../../store/teams/selector";
 import { Team } from "../../models/team/team";
+import { Equipment } from "../../models/equipment/equipment";
+import { loadEquipment } from "../../../store/equipment/actions";
+import { selectAllEquipments } from "../../../store/equipment/selector";
 
 @Component({
   selector: 'app-heroes',
@@ -24,14 +27,17 @@ import { Team } from "../../models/team/team";
 export class HeroesComponent implements OnInit {
   heroes = signal<Hero[]>([]);
   teams = signal<Team[]>([]);
+  equipments = signal<Equipment[]>([]);
   private store: Store<any> = inject(Store);
   private router: Router = inject(Router);
 
   ngOnInit(): void {
     this.store.dispatch(loadHeroes());
     this.store.dispatch(loadTeams());
+    this.store.dispatch(loadEquipment());
     this.store.select(selectAllHeroes).subscribe((heroes: Hero[]) => this.heroes.set(heroes));
     this.store.select(selectAllTeams).subscribe((teams: Team[]) => this.teams.set(teams));
+    this.store.select(selectAllEquipments).subscribe((equipments: Equipment[]) => this.equipments.set(equipments));
   }
 
   onItemChange(event: any): void {
@@ -42,12 +48,14 @@ export class HeroesComponent implements OnInit {
     return statusColorMap[status];
   }
 
-  heroesWithTeams = computed(() => {
+  heroesWithTeamsEquipment = computed(() => {
     const allTeams = this.teams();
+    const allEquipments = this.equipments();
     return this.heroes().map(hero => ({
       ...hero,
       teamName: allTeams.find(team => team.id === hero.teamId)?.name,
-      teamColor: allTeams.find(team => team.id === hero.teamId)?.color
+      teamColor: allTeams.find(team => team.id === hero.teamId)?.color,
+      equipments: allEquipments.filter(equipment => hero.equipmentIds.includes(equipment.id)).map(equipment => equipment.icon),
     }));
   });
 }

8f6fab3e32302f784fe1d0ad8f27fbac6af51b73
gideonn12 <gideoneeman@icloud.com>
2026-03-05 10:57:05 +0200

    Add Equipment Mock

---
+++ src/app/store/equipment/mock-equipment.ts
@@ -0,0 +1,41 @@
+import { Equipment } from '../../features/models/equipment/equipment';
+
+export const INITIAL_EQUIPMENT: Equipment[] = [
+  {
+    id: 1,
+    type: 'sword',
+    icon: '⚔️',
+    weight: 2.5,
+  },
+  {
+    id: 2,
+    type: 'shield',
+    icon: '🛡️',
+    weight: 5.0,
+  },
+  {
+    id: 3,
+    type: 'cape',
+    icon: '🧥',
+    weight: 1.0,
+  },
+  {
+    id: 4,
+    type: 'bow',
+    icon: '🏹',
+    weight: 1.5,
+  },
+  {
+    id: 5,
+    type: 'helmet',
+    icon: '⛑️',
+    weight: 3.0,
+  },
+  {
+    id: 6,
+    type: 'boots',
+    icon: '🥾',
+    weight: 2.0,
+  },
+];
+

4808bc26cb7631e3e397dd20727e2c0ce4af5682
gideonn12 <gideoneeman@icloud.com>
2026-03-10 15:30:40 +0200

    Implement Store For Team

--- src/app/features/services/teams.service.ts
+++ src/app/features/services/teams.service.ts
@@ -1,9 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Team } from '../models/team/team';
 import { selectAllTeams } from '../../store/teams/selector';
-import { loadTeams } from "../../store/teams/actions";
+import { loadTeams, updateTeam, deleteTeam, resetTeam } from "../../store/teams/actions";
 
 @Injectable()
 export class TeamsService {
@@ -12,5 +12,21 @@ export class TeamsService {
   getTeams(): Observable<Team[]> {
     this.store.dispatch(loadTeams());
     return this.store.select(selectAllTeams);
+  }
+
+  getTeam(id: number): Observable<Team> {
+    return this.store.select(selectAllTeams).pipe(map((teams: Team[]) => teams.find((team: Team) => team.id === id)!));
+  }
+
+  updateTeam(team: Team): void {
+    this.store.dispatch(updateTeam({ team }));
+  }
+
+  deleteTeam(id: number): void {
+    this.store.dispatch(deleteTeam({ id }));
+  }
+
+  resetTeam(id: number): void {
+    this.store.dispatch(resetTeam({ id }));
   }
 }

ce1d7c9b8c84aa20ee9facdefb92175a285879ff
gideonn12 <gideoneeman@icloud.com>
2026-03-10 16:37:24 +0200

    Add Circle Indicator If Equipment Is used & Add HeroName That Uses It

--- src/app/features/components/equipment/equipment.component.ts
+++ src/app/features/components/equipment/equipment.component.ts
@@ -1,25 +1,39 @@
-import {
-  Component,
-  inject,
-  OnInit,
-  signal,
-} from "@angular/core";
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Equipment } from "../../models/equipment/equipment";
 import { EquipmentService } from "../../services/equipment.service";
 import { UpperCasePipe } from "@angular/common";
+import { HeroService } from '../../services/hero.service';
+import { Hero } from '../../models/hero/hero';
+import { TeamsService } from '../../services/teams.service';
+import { Team } from '../../models/team/team';
 
 @Component({
   selector: "app-equipment",
   imports: [UpperCasePipe],
-  providers: [EquipmentService],
+  providers: [EquipmentService, HeroService, TeamsService],
   templateUrl: "./equipment.component.html",
   styleUrl: "./equipment.component.scss",
 })
 export class EquipmentComponent implements OnInit {
   equipments = signal<Equipment[]>([]);
+  heroes = signal<Hero[]>([]);
+  teams = signal<Team[]>([]);
   private equipmentService: EquipmentService = inject(EquipmentService);
+  private heroService: HeroService = inject(HeroService);
+  private teamsService: TeamsService = inject(TeamsService);
 
   ngOnInit(): void {
     this.equipmentService.getEquipment().subscribe((equipments: Equipment[]) => this.equipments.set(equipments));
+    this.heroService.getHeroes().subscribe((heroes: Hero[]) => this.heroes.set(heroes));
+    this.teamsService.getTeams().subscribe((teams: Team[]) => this.teams.set(teams));
   }
+
+  equipmentWithUsage = computed(() => {
+    return this.equipments().map((equipment: Equipment) => ({
+      ...equipment,
+      isUsed: this.heroes().some(hero => hero.equipmentIds.includes(equipment.id)),
+      usedBy: this.heroes().find(hero => hero.equipmentIds.includes(equipment.id))?.name,
+      color: this.teams().find(team => team.id === this.heroes().find(hero => hero.equipmentIds.includes(equipment.id))?.teamId)?.color ?? '#e65beb',
+    }));
+  });
 }

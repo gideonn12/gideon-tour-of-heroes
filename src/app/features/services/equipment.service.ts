bd71ccf19988d1b000168492d4ea4219fe5e5fc6
gideonn12 <gideoneeman@icloud.com>
2026-03-10 18:08:37 +0200

    Implement Equipment Store & Service

--- src/app/features/services/equipment.service.ts
+++ src/app/features/services/equipment.service.ts
@@ -1,9 +1,9 @@
 import { inject, Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { map, Observable } from 'rxjs';
 import { Equipment } from "../models/equipment/equipment";
 import { selectAllEquipments } from "../../store/equipment/selector";
-import { loadEquipment } from "../../store/equipment/actions";
+import { loadEquipment, updateEquipment, resetEquipment, deleteEquipment } from "../../store/equipment/actions";
 
 @Injectable()
 export class EquipmentService {
@@ -12,5 +12,21 @@ export class EquipmentService {
   getEquipments(): Observable<Equipment[]> {
     this.store.dispatch(loadEquipment());
     return this.store.select(selectAllEquipments);
+  }
+
+  getEquipment(id: number): Observable<Equipment> {
+    return this.store.select(selectAllEquipments).pipe(map((equipments: Equipment[]) => equipments.find((equipment: Equipment) => equipment.id === id)!));
+  }
+
+  updateEquipment(equipment: Equipment): void {
+    this.store.dispatch(updateEquipment({ equipment }));
+  }
+
+  deleteEquipment(id: number): void {
+    this.store.dispatch(deleteEquipment({ id }));
+  }
+
+  resetEquipment(id: number): void {
+    this.store.dispatch(resetEquipment({ id }));
   }
 }
